2e563faf236a95e4e91ea5d1bef0408398fa6ea4
Gwendolyn Wahl <wahl.gwen@gmail.com>
2020-11-13 13:17:36 -0800

    Improve start script

--- start.sh
+++ start.sh
@@ -3,12 +3,11 @@
 # exit when any command fails
 set -e
 
-cd /ardb/src/
-/ardb/src/ardb-server /ardb/src/ardb.conf >> ardb.log &
+mkdir /var/log/ardb
+mkdir /var/log/dynomite
 
-mkdir -p /var/log/dynomite/
-cd /dynomite/
+/ardb/src/ardb-server /ardb/ardb.conf >> /var/log/ardb/ardb.log &
 
-echo 'hello'
+/dynomite/src/dynomite --conf-file=/dynomite/conf/dynomite.yml -v11 -o /var/log/dynomite/dynomite.log &
 
-/dynomite/src/dynomite --conf-file=/dynomite/conf/single.yml -v11 -o /var/log/dynomite/dynomite_log.txt
+tail -F /var/log/ardb/ardb.log /var/log/dynomite/dynomite.log
